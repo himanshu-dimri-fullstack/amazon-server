e7e6d6c61735e2444b3d1480e38adf3fa488ba45
Himanshu Dimri <himanshudimrifullstack@gmail.com>
2026-03-23 14:16:49 +0530

    Add changes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
-const jsonServer = require('json-server');
-const path = require('path');
-const server = jsonServer.create();
+const jsonServer = require('json-server')
+const path = require('path')
+const server = jsonServer.create()
+const middlewares = jsonServer.defaults()
 
-const router = jsonServer.router(path.join(process.cwd(), 'db.json'));
-const middlewares = jsonServer.defaults();
+// db.json ka path sahi hona chahiye
+const router = jsonServer.router(path.join(process.cwd(), 'db.json'))
 
-server.use(middlewares);
-server.use(router);
+server.use(middlewares)
 
-module.exports = server;
+// Is line se saari requests handle hongi
+server.use(jsonServer.rewriter({
+    "/api/*": "/$1"
+}))
+server.use(router)
+
+// Vercel ke liye server ko listen nahi, export karna hota hai
+module.exports = server

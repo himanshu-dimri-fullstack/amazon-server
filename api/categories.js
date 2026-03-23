c8169b7f8ddbecf9236ff54cd3afe78835ef1996
Himanshu Dimri <himanshudimrifullstack@gmail.com>
2026-03-23 18:05:29 +0530

    Add changes

---
+++ api/categories.js
@@ -0,0 +1,7 @@
+const data = require("../db.json");
+
+export default function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
+    res.status(200).json(data.categories);
+}

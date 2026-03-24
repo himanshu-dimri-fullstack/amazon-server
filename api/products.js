445be3d1cce301a5967eaeb2b486cb47fb41eb85
Himanshu Dimri <himanshudimrifullstack@gmail.com>
2026-03-24 14:31:42 +0530

    add changes

--- api/products.js
+++ api/products.js
@@ -1,9 +1,24 @@
 const data = require("../db.json");
 
 export default function handler(req, res) {
+    const { subcategoryId, slug } = req.query;
+
+    let products = data.products;
+
+    if (subcategoryId) {
+        products = products.filter(
+            (item) => item.subcategoryId == subcategoryId
+        );
+    }
+
+    if (slug) {
+        products = products.filter(
+            (item) => item.slug === slug
+        );
+    }
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET");
 
-    res.status(200).json(data.products);
+    res.status(200).json(products);
 }
--- api/users.js
+++ api/users.js
@@ -1,7 +1,18 @@
 const data = require("../db.json");
 
 export default function handler(req, res) {
+    const { email } = req.query;
+
+    let users = data.users;
+
+    if (email) {
+        users = users.filter(
+            (user) => user.email === email
+        );
+    }
+
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET");
-    res.status(200).json(data.subcategories);
+
+    res.status(200).json(users);
 }
